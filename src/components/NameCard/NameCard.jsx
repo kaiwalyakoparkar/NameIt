e7d80f3c4af8ead6ca33fe74ea489e01522e6006
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2021-02-06 12:06:21 +0530

    Final touch up done

--- src/components/NameCard/NameCard.jsx
+++ src/components/NameCard/NameCard.jsx
@@ -4,7 +4,7 @@ import './NameCard.css'
 const nameCheapUrl = 'https://www.namecheap.com/domains/registration/results/?domain='
 const NameCard = ({ suggestedName }) => {
     return(
-        <a className="card-link" href={`${nameCheapUrl}${suggestedName}`}>
+        <a target="_blank" rel ="noreferrer" className="card-link" href={`${nameCheapUrl}${suggestedName}`}>
             <div className = "result-name-card">
                 <p className = "result-name">{suggestedName}
                 </p>
